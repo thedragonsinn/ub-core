aca60146df55795abd84b8f9be38dbd76cd9e4b9
thedragonsinn <98635854+thedragonsinn@users.noreply.github.com>
2024-05-19 22:53:40 +0530

    v1.3.4: bugfix task_id input in .c

--- ub_core/default_plugins/cancel.py
+++ ub_core/default_plugins/cancel.py
@@ -11,23 +11,28 @@ async def cancel_task(bot: BOT, message: Message) -> Message | None:
     USAGE: .c | .c -i <inline query id>
     """
     if "-i" in message.flags:
-        task_id: str = message.filtered_input
+        task_id: str = message.text_list[-1]
     else:
         task_id: str | None = message.replied_task_id
+
         if not task_id:
             await message.reply(
                 text="Reply To a Command or Bot's Response Message.", del_in=8
             )
             return
+
     all_tasks: set[asyncio.all_tasks] = asyncio.all_tasks()
     tasks: list[asyncio.Task] | None = [x for x in all_tasks if x.get_name() == task_id]
+
     if not tasks:
         await message.reply(text="Task not in Currently Running Tasks.", del_in=8)
         return
+
     response: str = ""
     for task in tasks:
         status: bool = task.cancel()
         response += (
-            f"Task: __{task.get_name()}__" f"\nCancelled: <code>{status}</code>\n"
+            f"Task: <code>{task.get_name()}</code>" f"\nCancelled: <code>{status}</code>\n"
         )
+
     await message.reply(response, del_in=5)
