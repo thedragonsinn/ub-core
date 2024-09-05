72955460f8b22d3f4c1287bef5cf66e59995e80b
thedragonsinn <98635854+thedragonsinn@users.noreply.github.com>
2024-09-05 18:28:11 +0530

    feat: make setup.py clean build cache

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
+import glob
+import os
+
 from setuptools import find_packages, setup
+
+__version__: str | None = None
 
 with open("requirements.txt", mode="r", encoding="utf-8") as req_file:
     requires: list[str] = req_file.readlines()
 
 with open("ub_core/version.py", mode="r", encoding="utf-8") as version_file:
     exec(version_file.read())
+
+
+def clean_up():
+    cache = [item for item in ("./build", "./*egg-info", "./dist") if glob.glob(item)]
+    os.system(" ".join(["rm -r", *cache]))
+
 
 setup(
     name="ub_core",
@@ -21,3 +32,5 @@ setup(
     install_requires=requires,
     scripts=["bin/run-ub-core"],
 )
+
+clean_up()
