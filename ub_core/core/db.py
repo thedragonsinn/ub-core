98e13dbc45d03fc8019ef7938d9955af36322ec3
thedragonsinn <98635854+thedragonsinn@users.noreply.github.com>
2024-09-09 12:43:49 +0530

    db.py feat: raise Keyerror on missing _id and add type hints.

--- ub_core/core/db.py
+++ ub_core/core/db.py
@@ -1,8 +1,9 @@
+import json
 import os
 from typing import Iterable
 
 import dns.resolver
-from motor.core import AgnosticClient, AgnosticDatabase
+from motor.core import AgnosticClient, AgnosticCollection, AgnosticDatabase
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
 from pymongo.results import DeleteResult, InsertOneResult, UpdateResult
 
@@ -28,7 +29,7 @@ class CustomDB(AsyncIOMotorCollection, Str):
     def __init__(self, collection_name: str):
         super().__init__(database=DB, name=collection_name)
 
-    async def add_data(self, data: dict) -> int | str:
+    async def add_data(self: AgnosticCollection, data: dict) -> int | str:
         """
         Add or Update Existing Data
 
@@ -36,18 +37,30 @@ class CustomDB(AsyncIOMotorCollection, Str):
             data: {"_id":id, rest of the data to be added/updated}
 
         Returns: Inserted Data ID if inserted else Modified Count
+
+        Raises: KeyError if _id is not present in data.
         """
-        found = await self.find_one({"_id": data["_id"]})
-        if not found:
+        unique_id_key = data.pop("_id", 0)
+
+        if not unique_id_key:
+            # Thanks @kakashi_htk [TG] | @ashwinstr [GitHub] for raising error
+            # suggestion.
+            raise KeyError(
+                f"Unique identifier key '_id' not found in data:{json.dumps(data, indent=4, ensure_ascii=False, default=str)}"
+            )
+
+        is_existing_entry = await self.find_one({"_id": unique_id_key})
+
+        if not is_existing_entry:
             entry: InsertOneResult = await self.insert_one(data)
             return entry.inserted_id
         else:
             entry: UpdateResult = await self.update_one(
-                {"_id": data.pop("_id")}, {"$set": data}
+                {"_id": unique_id_key}, {"$set": data}
             )
             return entry.modified_count
 
-    async def delete_data(self, id: int | str) -> int:
+    async def delete_data(self: AgnosticCollection, id: int | str) -> int:
         """
         Delete a DB Collection Entry
 
@@ -60,7 +73,7 @@ class CustomDB(AsyncIOMotorCollection, Str):
         delete_result: DeleteResult = await self.delete_one({"_id": id})
         return delete_result.deleted_count
 
-    async def increment(self, id: int, key: str, count: int) -> int:
+    async def increment(self: AgnosticCollection, id: int, key: str, count: int) -> int:
         """
         Increment a DB Entry Value for specified key.
 
@@ -75,7 +88,7 @@ class CustomDB(AsyncIOMotorCollection, Str):
         increment_result = await self.update_one({"_id": id}, {"$inc": {key: count}})
         return increment_result.modified_count
 
-    async def get_total(self, keys: Iterable) -> list[dict]:
+    async def get_total(self: AgnosticCollection, keys: Iterable) -> list[dict]:
         """
         Get Sum for key's value across the Collection
 
