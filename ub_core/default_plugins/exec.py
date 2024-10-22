6fbb72a96dee5408518631f22a2183bde6ce832a
thedragonsinn <98635854+thedragonsinn@users.noreply.github.com>
2024-10-22 17:32:31 +0530

    feat `exec.py`: new short var names for ease kf access

--- ub_core/default_plugins/exec.py
+++ ub_core/default_plugins/exec.py
@@ -10,8 +10,32 @@ import ub_core  # NOQA
 from ub_core import BOT, DB, DB_CLIENT, Cmd, Config, CustomDB, Message, bot  # NOQA
 from ub_core.utils import aio, shell  # NOQA
 
+# fmt: off
 
-async def executor(bot: BOT, message: Message) -> Message | None:
+def generate_locals(message: Message) -> tuple:
+    r = c = cid = u = uid = ru = ruid = None
+
+    c = getattr(message, "chat", None)
+    if c:
+        cid = message.chat.id
+
+    u = getattr(message, "from_user", None)
+    if u:
+        uid = message.from_user.id
+
+    r = getattr(message, "replied", None)
+    if r:
+        ru = r.from_user
+
+    if ru:
+        ruid = message.replied.from_user.id
+
+    return r, c, cid, u, uid, ru, ruid
+
+# fmt: on
+
+
+async def executor(bot: BOT, message: Message) -> None:
     """
     CMD: PY
     INFO: Run Python Code.
@@ -20,35 +44,50 @@ async def executor(bot: BOT, message: Message) -> Message | None:
         .py [-s] return 1
     """
     code: str = message.filtered_input.strip()
+
     if not code:
         return await message.reply("exec Jo mama?")
+
     reply: Message = await message.reply("executing")
+
     sys.stdout = codeOut = StringIO()
     sys.stderr = codeErr = StringIO()
-    # Indent code as per proper python syntax
-    formatted_code = "\n    ".join(code.splitlines())
+
     try:
         # Create and initialise the function
-        exec(f"async def _exec(bot, message):\n    {formatted_code}")
-        func_out = await asyncio.Task(
+        exec(
+            "async def _exec(bot: BOT, message: Message):"
+            + "\n    r, c, cid , u, uid, ru, ruid = generate_locals(message=message)"
+            + "\n    "
+            + "\n    ".join(code.splitlines()),
+        )
+        func_out = await asyncio.create_task(
             locals()["_exec"](bot, message), name=reply.task_id
         )
     except asyncio.exceptions.CancelledError:
-        return await reply.edit("`Cancelled....`")
+        await reply.edit("`Cancelled....`")
+        return
+
     except Exception:
         func_out = str(traceback.format_exc())
+
     sys.stdout = sys.__stdout__
     sys.stderr = sys.__stderr__
+
     output = codeErr.getvalue().strip() or codeOut.getvalue().strip()
+
     if func_out is not None:
         output = f"{output}\n\n{func_out}".strip()
+
     elif not output and "-s" in message.flags:
         await reply.delete()
         return
+
     if "-s" in message.flags:
         output = f">> ```\n{output}```"
     else:
         output = f"```python\n{code}```\n\n```\n{output}```"
+
     await reply.edit(
         output,
         name="exec.txt",
