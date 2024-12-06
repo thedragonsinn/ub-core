b8cae7fdf2d559e4117c274e21d7c2922d0b6ce0
thedragonsinn <98635854+thedragonsinn@users.noreply.github.com>
2024-12-06 11:49:32 +0530

    chore `core.logger`: dont log OSErrors caused by network issues

--- ub_core/core/logger.py
+++ ub_core/core/logger.py
@@ -6,13 +6,14 @@ from logging import (
     INFO,
     WARNING,
     Handler,
+    LogRecord,
     StreamHandler,
     basicConfig,
     getLogger,
     handlers,
 )
 
-from ub_core import Config, bot
+from ub_core import Config, Message, bot
 
 os.makedirs(name="logs", exist_ok=True)
 
@@ -24,7 +25,7 @@ class TgErrorHandler(Handler):
     A Custom Logging Handler to Log Error and Above Events in TG LOG CHANNEL.
     """
 
-    def emit(self, log_record):
+    def emit(self, log_record: LogRecord):
         try:
             self.format(log_record)
             self.log_to_tg(log_record)
@@ -32,27 +33,36 @@ class TgErrorHandler(Handler):
             print(e)
 
     @staticmethod
-    def log_to_tg(log_record):
+    def log_to_tg(log_record: LogRecord):
         if not (bot.client.is_connected and bot.is_idling):
             return
 
-        try:
-            chat = (
-                log_record.tg_message.chat.title
-                or log_record.tg_message.chat.first_name
-            )
-        except BaseException:
-            chat = ""
+        tg_message = getattr(log_record, "tg_message", None)
+        if isinstance(tg_message, Message):
+            chat = tg_message.chat
+            chat_name: str = chat.title or chat.first_name
+            chat_id: int = chat.id
+        else:
+            chat_name, chat_id = "", 0
+
+        error_message: str = log_record.exc_text or log_record.message
+
+        if (
+            log_record.funcName in ("handler_worker", "run")
+            and "OSError:" in error_message
+        ):
+            return
 
         text = (
             f"#{log_record.levelname} #TRACEBACK"
-            f"<b>\nChat</b>: {chat}"
+            f"<b>\nChat</b>: <code>{chat_name}</code> [<code>{chat_id}</code>]"
             f"\n<b>Line No</b>: <code>{log_record.lineno}</code>"
             f"\n<b>Func</b>: <code>{log_record.funcName}</code>"
             f"\n<b>Module</b>: <pre language=python>{log_record.pathname}</pre>"
             f"\n<b>Time</b>: <code>{log_record.asctime}</code>"
-            f"\n<b>Error Message</b>:\n<pre language=python>{log_record.exc_text or log_record.message}</pre>"
+            f"\n<b>Error Message</b>:\n<pre language=python>{error_message}</pre>"
         )
+
         asyncio.run_coroutine_threadsafe(
             coro=bot.client.log_text(text=text, name="traceback.txt"), loop=bot.loop
         )
