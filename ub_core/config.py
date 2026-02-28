dc200e4837ad661f6a7ef596a1798e2a1d77c39e
thedragonsinn <98635854+thedragonsinn@users.noreply.github.com>
2026-02-28 22:21:20 +0530

    `config.py:` switch to task manager and change sudo keywords in `Cmd`

--- ub_core/config.py
+++ ub_core/config.py
@@ -1,11 +1,12 @@
-import asyncio
 import importlib
 import json
 import logging
-from collections.abc import Callable, Coroutine
-from os import environ, path, sep
+from collections.abc import Callable
+from os import getenv, path, sep
 
 from git import InvalidGitRepositoryError, Repo
+
+from .task_manager import TaskManager
 
 LOGGER = logging.getLogger("Config")
 
@@ -26,70 +27,68 @@ def update_extra_config():
 
 
 class Cmd:
-    def __init__(self, cmd: str, func: Callable, cmd_path: str, sudo: bool):
+    def __init__(self, cmd: str, func: Callable, cmd_path: str, allow_sudo: bool):
         self.cmd: str = cmd
         self.cmd_path: str = cmd_path
         self.dir_name: str = path.basename(path.dirname(cmd_path))
         self.doc: str = func.__doc__ or "Not Documented."
         self.func: Callable = func
         self.is_from_core: bool = "ub_core" in cmd_path
-        self.loaded = False
-        self.sudo: bool = sudo
+        self.loaded_for_sudo = False
+        self.allow_sudo: bool = allow_sudo
 
-    def __str__(self):
+    def __str__(self) -> str:
         return json.dumps(self.__dict__, indent=4, ensure_ascii=False, default=str)
 
 
 class Config:
-    BOT_NAME = environ.get("BOT_NAME", "BOT")
+    TASK_MANAGER = TaskManager()
 
-    BACKGROUND_TASKS: list[asyncio.Task] = []
+    BOT_NAME = getenv("BOT_NAME", "BOT")
 
     CMD_DICT: dict[str, Cmd] = {}
 
-    CMD_TRIGGER: str = environ.get("CMD_TRIGGER", ".")
+    CMD_TRIGGER: str = getenv("CMD_TRIGGER", ".")
 
-    DEV_MODE: int = int(environ.get("DEV_MODE", 0))
+    COMMAND_LOG_LEVEL: int = int(getenv("COMMAND_LOG_LEVEL", 0))
 
-    DISABLED_SUPERUSERS: list[int] = []
+    DEV_MODE: int = int(getenv("DEV_MODE", 0))
 
-    EXIT_TASKS: list[Callable] = []
-
-    INIT_TASKS: list[Coroutine] = []
+    DISABLED_SUPERUSERS: set[int] = set()
 
     INLINE_QUERY_CACHE: dict[str | int, dict] = {}
 
     INLINE_RESULT_CACHE: set[str] = set()
 
-    LOG_CHAT: int = int(environ.get("LOG_CHAT", 0))
+    LOG_CHAT: int = int(getenv("LOG_CHAT", 0))
 
-    LOG_CHAT_THREAD_ID: int = int(environ.get("LOG_CHAT_THREAD_ID", 0)) or None
+    LOG_CHAT_THREAD_ID: int = int(getenv("LOG_CHAT_THREAD_ID", 0)) or None
 
     LOAD_HANDLERS: bool = True
 
     MODE: str = "dual"
 
-    OWNER_ID: int = int(environ.get("OWNER_ID", 0))
+    OWNER_ID: int = int(getenv("OWNER_ID", 0))
 
     try:
         REPO: Repo = Repo(".")
-        EXIT_TASKS.append(REPO.close)
+        TASK_MANAGER.create_task(REPO.close, task_type="exit")
     except InvalidGitRepositoryError:
         REPO = None
 
     SUDO: bool = False
 
-    SUDO_TRIGGER: str = environ.get("SUDO_TRIGGER", "!")
+    SUDO_TRIGGER: str = getenv("SUDO_TRIGGER", "!")
 
-    SUDO_USERS: list[int] = []
+    SUDO_USERS: set[int] = set()
 
-    SUPERUSERS: list[int] = []
+    SUPERUSERS: set[int] = set()
 
-    UPSTREAM_REPO: str = environ.get("UPSTREAM_REPO", "")
+    UPSTREAM_REPO: str = getenv("UPSTREAM_REPO", "")
 
     UPDATE_REPO = "https://github.com/thedragonsinn/ub-core"
 
-    WORKING_DIR = environ.get("WORKING_DIR", "app")
+    WORKING_DIR = getenv("WORKING_DIR", "app")
 
 
 update_extra_config()
