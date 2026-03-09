cd0f8a8b42e34bee0188fbbf7bb8a68d84f5f0d1
thedragonsinn <98635854+thedragonsinn@users.noreply.github.com>
2026-03-09 21:45:14 +0530

    deadlock fix in `task_manager` and `@bot.register_task`

--- ub_core/core/decorators/register_worker.py
+++ ub_core/core/decorators/register_worker.py
@@ -7,16 +7,16 @@ class RegisterWorker:
     @staticmethod
     def register_worker(
         interval: int,
-        break_condition: Callable = None,
         name: str = None,
+        break_condition: Callable = None,
         ignore_if_exists: bool = True,
     ):
         def inner(function: Callable):
             _name = name or f"{function.__name__}-worker"
 
-            tasks_with_same_name = Config.TASK_MANAGER.get_tasks(_name, "workers")
+            tasks_with_same_name = set(Config.TASK_MANAGER.get_tasks(_name, "workers"))
 
-            if tasks_with_same_name:
+            if len(tasks_with_same_name) > 1:
                 if ignore_if_exists:
                     return function
 
