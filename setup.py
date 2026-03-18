1c09dc186ae27cc1caf84902d70286feb0790972
thedragonsinn <98635854+thedragonsinn@users.noreply.github.com>
2026-03-18 20:13:01 +0530

    `setup.py`: switch to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,46 +1,21 @@
 import glob
 import os
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 __version__: str | None = None
-
+vlocals = {}
 
 with open("ub_core/version.py", encoding="utf-8") as version_file:
-    exec(version_file.read())
+    exec(version_file.read(), vlocals)
 
 
-def get_platform_requirements() -> list[str]:
-    with open("requirements.txt", encoding="utf-8") as req_file:
-        packages: list[str] = list(
-            filter(lambda l: not l.startswith("#") and l != "\n", req_file.readlines())
-        )
-
-    if "termux" not in os.environ.get("HOME", ""):
-        return packages
-
-    return list(filter(lambda p: not p.startswith(("uvloop", "psutil", "pillow")), packages))
+setup(version=vlocals["__version__"])
 
 
 def clean_up():
     cache = [item for item in ("./build", "./*egg-info", "./dist") if glob.glob(item)]
     os.system(" ".join(["rm -r", *cache]))
 
-
-setup(
-    name="ub_core",
-    version=__version__,
-    author="Meliodas",
-    description="BoilerPlate Code for Bot projects.",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires="~=3.11",
-    install_requires=get_platform_requirements(),
-    scripts=["bin/run-ub-core"],
-)
 
 clean_up()
